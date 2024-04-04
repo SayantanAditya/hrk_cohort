4c4549bd533e2a3001cbfe4c1ae7c4e2c40b610d
SayantanAditya <sayantanaditya@gmail.com>
2024-04-04 16:23:53 +0530

    axios fixed

--- week6.2/src/App.jsx
+++ week6.2/src/App.jsx
@@ -1,45 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      axios.get('https://sum-server.100xdevs.com/todo')
-        .then((res) => {
-          if (Array.isArray(res.data.todos)) {
-            setTodos(res.data.todos);
-          } else {
-            console.error('Todos is not an array:', res.data.todos);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching todos:', error);
-        });
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId); // Cleanup interval on component unmount
-    };
-  }, []);
-
-  return (
-    <>
-      {todos.map(({ title, description }, index) => (
-        <Todo key={index} title={title} description={description} />
-      ))}
-    </>
-  );
-}
-
-function Todo({ title, description }) {
   return (
     <div>
-      <h2>{title}</h2>
-      <h4>{description}</h4>
+      <Todo id={1} />
     </div>
-  );
+  )
 }
 
-export default App;
+function Todo({id}){
+  const [todo,setTodo] = useState([])
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
+      .then( function(res) {
+        setTodo (res.data.todo);
+      })
+  }, [])
+
+  return <div>
+  <h1>
+    {todo.title}
+  </h1>
+  <h4>
+    {todo.description}
+  </h4>
+</div>
+}
+export default App
